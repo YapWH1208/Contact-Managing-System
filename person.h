b728f76001e0ad476e1aa341e78f20dd541d84ec
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-08-09 18:27:50 +0800

    commit person
    Added person struct

---
+++ person.h
@@ -0,0 +1,16 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+
+struct person {
+    string name;
+    int sex;
+    int age;
+    string phone_num;
+    string address;
+};
+
+#endif // PERSON_H
+
