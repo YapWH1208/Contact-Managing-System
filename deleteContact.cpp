1c4285050ce4ddd983d3c5117dc492867efb63b5
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-08-10 10:51:28 +0800

    fixes on libraries importing

--- deleteContact.cpp
+++ deleteContact.cpp
@@ -1,16 +1,8 @@
 # include "deleteContact.h"
 # include "addressbook.h"
+# include "isExist.h"
 
 using namespace std;
-
-int isExist(addressbook *abs, string name){
-    for(int i=0; i<abs->size; i++){
-        if(abs->personArray[i].name == name){
-            return i;
-        }
-    }
-    return -1;
-}
 
 void deleteContact(addressbook *abs){
     cout << "Enter name to delete: " << endl;
--- searchContact.cpp
+++ searchContact.cpp
@@ -1,21 +1,22 @@
 # include <iostream>
 # include "searchContact.h"
 # include "addressbook.h"
+# include "isExist.h"
 
 using namespace std;
 
-void searchContact(addressbook &abs){
+void searchContact(addressbook *abs){
     cout << "Enter name: " << endl;
     string name;
     cin >> name;
 
     int ret = isExist(abs, name);
     if(ret != -1){
-        cout << "Name: " << abs.personArray[ret].name << endl;
-        cout << "Sex: " << abs.personArray[ret].sex << endl;
-        cout << "Age: " << abs.personArray[ret].age << endl;
-        cout << "Phone Number: " << abs.personArray[ret].phone_num << endl;
-        cout << "Address: " << abs.personArray[ret].address << endl;
+        cout << "Name: " << abs->personArray[ret].name << endl;
+        cout << "Sex: " << abs->personArray[ret].sex << endl;
+        cout << "Age: " << abs->personArray[ret].age << endl;
+        cout << "Phone Number: " << abs->personArray[ret].phone_num << endl;
+        cout << "Address: " << abs->personArray[ret].address << endl;
     }
     else{
         cout << "Name not found" << endl;
